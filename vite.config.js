b54e3d0b583f76b4b96ab793d16b46b58e18924b
microblinde <nikita.budankov@gmail.com>
2026-06-26 15:17:25 +0300

    debag

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import legacy from '@vitejs/plugin-legacy';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    legacy({
-      targets: ['ios >= 13'],
-    }),
-  ],
-  base: process.env.VITE_BASE_PATH || './',
+  plugins: [react()],
+  base: '/',
+  build: {
+    target: 'es2015',
+  },
 });
