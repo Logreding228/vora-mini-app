57414511f2e1bc9e4197d5947a9b1494d95c1523
microblinde <nikita.budankov@gmail.com>
2026-06-26 14:54:08 +0300

    feat: add legacy plugin for iOS support

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import legacy from '@vitejs/plugin-legacy';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    legacy({
+      targets: ['ios >= 13'],
+    }),
+  ],
   base: process.env.VITE_BASE_PATH || './',
 });
